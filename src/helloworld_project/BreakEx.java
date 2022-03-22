d32791cdcd3dadf4f5c17b644f546be09f58e807
tmscrown <nadocod2r@gmail.com>
2022-03-22 11:05:10 +0900

    5th commit

---
+++ src/helloworld_project/BreakEx.java
@@ -0,0 +1,18 @@
+package helloworld_project;
+
+import java.util.Scanner;
+
+public class BreakEx {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("exit를 입력하면 종료합니다.");
+		while(true) {
+			System.out.print(">>");
+			String text=sc.nextLine();
+			if(text.equals("exit"))
+				break;
+		}
+		System.out.println("종료합니다......");
+	}
+}
