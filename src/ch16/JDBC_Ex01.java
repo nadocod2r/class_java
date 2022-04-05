290dff3d1f94531a8a8af9c316b19205f219b61d
tmscrown <nadocod2r@gmail.com>
2022-04-05 09:11:58 +0900

    jdbc fixed commit

--- src/ch16/JDBC_Ex01.java
+++ src/ch16/JDBC_Ex01.java
@@ -5,43 +5,47 @@ import java.sql.*;
 public class JDBC_Ex01 {
 	public static void main(String[] args) {
 		
-		String driverName="com.mysql.jdbc.Driver";
-		String url="jdbc:mysql://localhost:3306/student_db";
-		String username="root";
-		String password="1234";
-		String query="select * from smembers";
-//		String query2="delete from smembers where id='tiger'";
-		String query2="insert into smembers (num,name,id,pw,email) values (108,'aaa','aaa','1234','aaa@gmail.com')";
+//		String driverName="com.mysql.jdbc.Driver";
+		String driver="com.mysql.cj.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/student";
+		String id="root";
+		String pw="1234";
+		String sql="select * from smembers";
+//		String sql2="delete from smembers where id='tiger'";
+//		String sql2="insert into smembers (num,name,id,pw,email) values (108,'aaa','aaa','1234','aaa@gmail.com')";
+//		String sql2="delete from smembers where num=108";
+		String sql2="update smembers set pw='1234' where num=2";
 				
 		
-		ResultSet ret=null;
 		
 		Connection conn=null;
 		Statement stmt=null;
+		ResultSet rs=null;
 		
 		try {
-			Class.forName(driverName); // mysql jdbc 드라이버 호출
-			conn = DriverManager.getConnection(url, username,password);
+			Class.forName(driver); // mysql jdbc 드라이버 호출
+			conn = DriverManager.getConnection(url, id, pw);
 			System.out.println("DB 연결 완료!!!");
 			stmt=conn.createStatement(); //SQL 실행용 statement 객체 생성
-			ret=stmt.executeQuery(query); //SQL 실행-반환되는 결과값을 ResultSet으로 받음
+			rs=stmt.executeQuery(sql); //SQL 실행-반환되는 결과값을 ResultSet으로 받음
 			
-			while(ret.next()) {
-				String pid=ret.getString("id");
-				String ppw=ret.getString("pw");
-				System.out.println(pid);
-				System.out.println(ppw);
+			while(rs.next()) {
+				int pnum=rs.getInt("num");
+				String pid=rs.getString("id");
+				String ppw=rs.getString("pw");
+				System.out.println(pnum+". "+pid+" "+ppw);
+//				System.out.println(ppw);
 				
 			}
 			
-			int rnum=stmt.executeUpdate(query2);
+			int rnum=stmt.executeUpdate(sql2);
 			
 			if (rnum==1) {
 //				System.out.println("회원탈퇴성공!!!");
-				System.out.println("회원가입성공!!!");
+				System.out.println("성공!!!");
 			}else {
 //				System.out.println("회원삭제성공");
-				System.out.println("회원가입실패");
+				System.out.println("실패");
 			}
 			
 			
